f95d39a558e1fc4dcbecf0c48427ce127795e4f2
Luis <feliperojas12@hotmail.com>
2025-09-11 20:38:14 -0700

    updated

--- auth-app/util/auth.js
+++ auth-app/util/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyBgXLbdwaMqNoV8tWLm5xhUcris7mR1bZc';
+const API_KEY = 'my_key';
 
 async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
